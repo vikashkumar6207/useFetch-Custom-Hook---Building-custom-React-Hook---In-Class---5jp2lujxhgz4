4c8465b57566333dcf357a13d0befa332af46e33
vikashkumar6207 <vktd828403@gmail.com>
2024-06-23 06:42:31 +0530

    don

--- components/useFetch.js
+++ components/useFetch.js
@@ -1,3 +1,24 @@
-//write your code here
 
-export default useFetch
+import React, { useEffect, useState } from "react";
+
+export default function useFetch({ url }) {
+  const [data, setData] = useState('');
+  const [loading, setLoding] = useState(false);
+  const [error, setError] = useState(false);
+  console.log(data);
+  useEffect(() => {
+    setLoding(true);
+
+    fetch(url)
+      .then((res) => res.json())    
+      .then((res) => { 
+        if(res.error){ setError(true)
+        }else{
+        setData(res)
+        }
+    })
+      .catch((err)=> setError(true))
+      .finally(() => setLoding(false));
+  }, []);
+  return{ data, loading, error };
+}
--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,14 @@ import useFetch from '../components/useFetch';
 function Home() {
   const { data, loading, error } = useFetch("https://content.newtonschool.co/v1/pr/63b6c911af4f30335b4b3b89/products?limit=10&page=1");
 
+  console.log(data);
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
   return (
     <div>
       <h1>Products</h1>
-      {/* {document.write(data)} */}
+      {/* document.write(data) */}
       <ul>
       {data && data.map(item => (
         <li key={item.id}>{item.title}</li>
