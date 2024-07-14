38fce19ae8aa10d4e6d5d256655339ec4edcdb79
vikashkumar6207 <vktd828403@gmail.com>
2024-07-14 07:38:15 +0530

    done

--- components/useFetch.js
+++ components/useFetch.js
@@ -1,3 +1,28 @@
 //write your code here
+"use client"
+import React, { useEffect, useState } from "react";
 
-export default useFetch
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoding] = useState(false);
+  const [error, setError] = useState(false);
+ 
+  useEffect(() => {
+    setLoding(true);
+
+    fetch(url)
+      .then((res) => res.json())    
+      .then((res) => { 
+        if(res.error){ setError(true)
+        }else{
+        setData(res.data);
+        
+        }
+    })
+      .catch((err)=> setError(true))
+      .finally(() => setLoding(false));
+  }, []);
+  return{ data , loading, error };
+}
+
+export default useFetch;
